c9267ee67509a2e03ff17e64de2da27a146f6e11
zacwillmington <zac.willmington@gmail.com>
2019-06-09 19:59:20 -0500

    Added html to base.html.twig file and requre bootstrap CDN.

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="welcome")
+     */
+    public function index()
+    {
+        return $this->render('welcome/index.html.twig', [
+            'controller_name' => 'WelcomeController',
+        ]);
+    }
+}
